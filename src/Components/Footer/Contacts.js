e777f2035405f90891cf36d6a9e3182d7db49851
Demoferm <60359246+Demoferm@users.noreply.github.com>
2020-04-05 15:58:10 -0400

    Message

--- src/Components/Footer/Contacts.js
+++ src/Components/Footer/Contacts.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Contacts() {
     return (
      <>
-     <p className='contacts'>Contacts: </p>
+     <p align='left'>Contacts: </p>
         <p align='left'> <a href='mailto:d.borodastov@gmail.com'>d.borodastov@gmail.com</a></p>
         </>
     )
