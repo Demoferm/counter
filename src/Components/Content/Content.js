d196c11b72ec92194e63e049b5ada406f434030e
Demoferm <60359246+Demoferm@users.noreply.github.com>
2020-04-27 17:30:18 -0400

    fixed 2 to 10

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -55,42 +55,7 @@ function Content(props) {
                     </button>
                 </p>
             </div>
-            {/*<div className='col-4'>*/}
-            {/*    <Logo/>*/}
-            {/*</div>*/}
-            {/*<div className='col-8'>*/}
-            {/*    <h1 className='h1'>Programming Academy in Silicon Valley</h1>*/}
-            {/*    <h2>What is Lorem Ipsum?</h2>*/}
-            {/*    <p className='text'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem*/}
-            {/*        Ipsum has*/}
-            {/*        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley*/}
-            {/*        of type*/}
-            {/*        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the*/}
-            {/*        leap*/}
-            {/*        into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with*/}
-            {/*        the*/}
-            {/*        release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop*/}
-            {/*        publishing*/}
-            {/*        software like Aldus PageMaker including versions of Lorem Ipsum. </p>*/}
-            {/*</div>*/}
-            {/*<div className='col-4' align='right'>*/}
-            {/*    <p>Enter your name:</p>*/}
-            {/*    <p><input type='text' id='name' color='red' placeholder='write here...'/>*/}
-            {/*        <button onClick={() => {*/}
-            {/*            props.onButtonClick(document.getElementById('name').value)*/}
-            {/*        }}>OK*/}
-            {/*        </button>*/}
-            {/*        <button onClick={() => {*/}
-            {/*            document.getElementById('name').value = '';*/}
-            {/*        }}>CANCEL*/}
-            {/*        </button>*/}
-            {/*    </p>*/}
-            {/*</div>*/}
-            {/*<div align='center' className='col-8'>*/}
-            {/*    <p>{props.MenuButtons.map(el => <button key={el} onClick={() => {*/}
-            {/*        props.onButtonClick(el)*/}
-            {/*    }}>{el}</button>)}</p>*/}
-            {/*    <div className='row'>*/}
+
                     <div className='col-12'>
                         <h2 align='center'>Calculator</h2>
                     </div>
